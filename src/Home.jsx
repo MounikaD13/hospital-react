2244bc153e3cccd08451bafe244465f7369ac668
MounikaD13 <mounika.d3945@gmail.com>
2025-10-25 10:27:41 +0530

    last

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,43 +2,44 @@ import React from 'react'
 import './Home.css'
 export default function Home() {
   return (
-      <div>
-        <div c>
-          <img width="100%" src="https://static.vecteezy.com/system/resources/previews/003/598/202/large_2x/doctor-consultation-flat-color-infographic-template-family-health-treatment-poster-with-text-ppt-page-concept-design-with-cartoon-characters-creative-data-visualization-info-banner-idea-vector.jpg" alt="" />
+    <div>
+      <div class='image'>
+        <h1>Making Health Care Better Together</h1>
+        <p>In healthcare sector,service excellence is the facility of the hospital as healthcare service provider to consistently </p>
+      </div>
+      <div class="everyday text-center mt-4">
+        <h2>Why Choose Us</h2>
+      </div>
+      <div class="row everyday text-center g-2">
+        <div class="col-md-3">
+          <div class="cardlast">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3-0yXpqcuerfAHI0V4Is0lHs5op07nuVkuw&s" alt="" />
+            <h5>Experience</h5>
+            <p>2L+operations</p>
+          </div>
         </div>
-        <div class="everyday text-center mt-4">
-          <h2>Why Choose Us</h2>
+        <div class="col-md-3">
+          <div class="cardlast">
+            <img src="https://www.shutterstock.com/image-vector/expert-icon-element-design-260nw-2657938471.jpg" alt="" />
+            <h5>Expertise</h5>
+            <p>Over 25 years of practice</p>
+          </div>
         </div>
-        <div class="row everyday text-center g-2">
-          <div class="col-md-3">
-            <div class="cardlast">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3-0yXpqcuerfAHI0V4Is0lHs5op07nuVkuw&s" alt="" />
-              <h5>Experience</h5>
-              <p>2L+operations</p>
-            </div>
+        <div class="col-md-3">
+          <div class="cardlast">
+            <img src="https://2andararq.com.br/wp-content/uploads/2024/07/experiencia.png" alt="" />
+            <h5>Awards</h5>
+            <p>Best Hospital in the city for 5 yrs</p>
           </div>
-          <div class="col-md-3">
-            <div class="cardlast">
-              <img src="https://www.shutterstock.com/image-vector/expert-icon-element-design-260nw-2657938471.jpg"alt="" />
-              <h5>Expertise</h5>
-              <p>Over 25 years of practice</p>
-            </div>
-          </div>
-           <div class="col-md-3">
-            <div class="cardlast">
-              <img src="https://2andararq.com.br/wp-content/uploads/2024/07/experiencia.png"alt="" />
-              <h5>Awards</h5>
-              <p>Best Hospital in the city for 5 yrs</p>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="cardlast">
-              <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRj1hunpm-A9bQwvMTnDCLFIBXRPj3KTEtq5IKGTh_GHKO1_VOC" alt="" />
-              <h5>Range</h5>
-              <p>Simple to Complex</p>
-            </div>
+        </div>
+        <div class="col-md-3">
+          <div class="cardlast">
+            <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRj1hunpm-A9bQwvMTnDCLFIBXRPj3KTEtq5IKGTh_GHKO1_VOC" alt="" />
+            <h5>Range</h5>
+            <p>Simple to Complex</p>
           </div>
         </div>
       </div>
+    </div>
   )
 }
